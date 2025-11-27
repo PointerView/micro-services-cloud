06347e10d10339c0a65f927de2e39d47b75ea9c2
danihc <danielhc132000@gmail.com>
2025-11-27 10:38:39 +0100

    Agregando servidor de recursos

---
+++ gateway-service/src/test/java/com/pointerview/springcloud/gateway_service/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pointerview.springcloud.gateway_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
