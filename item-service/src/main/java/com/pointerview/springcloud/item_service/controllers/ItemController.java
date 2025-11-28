336a3b4836970e6020a8320f9e3e020a9a648c0b
danihc <danielhc132000@gmail.com>
2025-11-28 11:38:34 +0100

    Completado config server aplicado a servicios y agregado refresh scope para actualizacion fantasma del componente

--- item-service/src/main/java/com/pointerview/springcloud/item_service/controllers/ItemController.java
+++ item-service/src/main/java/com/pointerview/springcloud/item_service/controllers/ItemController.java
@@ -6,16 +6,18 @@ import com.pointerview.springcloud.item_service.services.ItemService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
-import java.util.Collections;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
+@RefreshScope // Para refrescar el componente con el servidor activo
 @RestController
 public class ItemController {
 
@@ -25,15 +27,38 @@ public class ItemController {
     @Autowired
     private CircuitBreakerFactory circuitBreakerFactory;
 
+    @Value("${configuracion.texto}")
+    private String text;
+
+    @Value("${server.port}")
+    private String port;
+
+    @Autowired
+    private Environment env; // Alternativa a @Value()
+
     private final Logger log = LoggerFactory.getLogger(ItemController.class);
 
     @GetMapping
     public ResponseEntity<List<Item>> findAll(@RequestParam(required = false) String name,
                                               @RequestHeader(name = "token-request") String token) {
 
-        System.out.println(name);
         System.out.println(token);
+        System.out.println(text);
         return ResponseEntity.ok(itemService.findAll());
+    }
+
+    @GetMapping("/fetch-config")
+    public ResponseEntity<?> fetchConfigs() {
+        Map<String, String> output = new HashMap<>();
+        output.put("port", port);
+        output.put("text", text);
+
+        if(env.getActiveProfiles().length > 0 && env.getActiveProfiles()[0].equals("dev")){
+            output.put("autor.nombre", env.getProperty("configuracion.autor.name"));
+            output.put("autor.email", env.getProperty("configuracion.autor.email"));
+        }
+
+        return ResponseEntity.ok(output);
     }
 
     @GetMapping("/{id}")
